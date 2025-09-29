15d2c7f8fb9a0ca3039a7edcfc17bb55da7cafaa
1080444-Airto <1080444@hr.nl>
2025-09-29 11:16:48 +0200

    check if send to github

--- src/games/competitors.vue
+++ src/games/competitors.vue
@@ -12,7 +12,7 @@
                 </div>
                 <div class="body__competitors">
                     <p v-for="player in this.$store.state.players" class="body__competitors--names">
-                        {{ player.name }}frewiof
+                        {{ player.name }}
                     </p>
                 </div>
             </div>
