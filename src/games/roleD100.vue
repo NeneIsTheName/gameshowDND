c4da57affbae180ddf92a6b47057de016ff22509
NeneIsTheName <1080444@hr.nl>
2025-10-06 14:28:53 +0200

    added css and computed players

--- src/games/roleD100.vue
+++ src/games/roleD100.vue
@@ -1,17 +1,30 @@
 <template>
     <div class="container">
+        <section class="header-roleD100">
+            <div class="header-roleD100__title">
+                <p class="header-roleD100__title--text">Role D100</p>
+            </div>
+        </section>
         <div class="players-roleD100">
-            <section v-for="player in this.$store.state.players" class="role-card">
+            <section v-for="player, index in this.players" class="role-card">
                 <div class="player-details">
                     <div class="player-details__name">
                         <p class="player-details__name--text">{{ player.name }}</p>
+                        <p>{{ playerGold[player.id] }}</p>
                     </div>
-                </div>
-                <div class="player-details__number">
-                    <p class="player-details__number--text" :id="`randomNumber${player.id}`">0</p>
+                    <div class="player-details__number">
+                        <p class="player-details__number--text" :id="`randomNumber${player.id}`">0</p>
+                    </div>
+                    <div class="player-details__check">
+                        <button class="player-details__check--keep" @click="keepGold(player.id)">Keep Gold</button>
+                        <button class="player-details__check--lost" @click="lostGame(player.id)">Lost Game</button>
+                    </div>
                 </div>
             </section>
         </div>
+        <section class="">
+
+        </section>
         <section class="generator-card">
             <div class="between">
                 <div class="between__number">
@@ -21,7 +34,7 @@
                     <p class="between__number--text">and</p>
                 </div>
                 <div class="between__number">
-                    <input class="between__number--input" type="text" v-model="between.max">
+                    <input v-model="between.max" class="between__number--input" type="text">
                 </div>
             </div>
             <div class="generate">
@@ -36,22 +49,37 @@
 
 <script>
 export default{
-    name: "competitors",
+    name: "roleD100",
     data(){
         return {
             between: { min: 0, max: 0 },
+            playerGold: []
         }
+    },
+    computed: {
+        players() {
+            return this.$store.state.players
+        }
+    },
+    mounted(){
+        this.players.forEach((player, index) => {
+            this.playerGold[player.id] = 0
+        });
+        console.log(this.playerGold)
     },
     methods: {
         generateNumber(){
             if(!isNaN(this.between.min) && !isNaN(this.between.max)){
-                this.$store.state.players.forEach(player => {
-                    document.getElementById(`randomNumber${player.id}`).innerText = Math.floor(Math.random() * (this.between.max - this.between.min + 1)) + Number(this.between.min)
+                this.players.forEach((player, index) => {
+                    document.getElementById(`randomNumber${index}`).innerText = Math.floor(Math.random() * (this.between.max - this.between.min + 1)) + Number(this.between.min)
                 });
             }
             else{
                 alert("Please enter a valid number")
             }
+        },
+        keepGold(playerID){
+            this.$store.commit('addPlayerGold', playerID)
         }
     }
 }
--- src/store.js
+++ src/store.js
@@ -22,7 +22,7 @@ const store = createStore({
                     })
                 }
             });
-        }
+        },
     },
 
     plugins: [
