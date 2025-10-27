c635433ff4b94dcc3b08bd1f6769eb4c5a421ea3
NeneIsTheName <1080444@hr.nl>
2025-10-27 15:52:53 +0100

    steal or deal front-end

--- src/games/stealOrDeal.vue
+++ src/games/stealOrDeal.vue
@@ -3,7 +3,21 @@
 
 <main class="main">
     <section class="stealOrDeal">
-        <div class="">
+        <div class="game">
+            <div class="game__vs" v-for="pair in pairedPlayers.pairs">
+                <p class="game__vs--player">{{ pair[0].name }} <span v-if="showGold">| Gold: {{ pair[0].gold }}</span></p>
+                <p class="game__vs--player">{{ pair[1].name }} <span v-if="showGold">| Gold: {{ pair[1].gold }}</span></p>
+            </div>
+            <div class="game__special" v-if="pairedPlayers.special_player">
+                <p class="game__special--title">Special Game:</p>
+                <p class="game__special--player">
+                    {{ pairedPlayers.special_player.name }}
+                    <span v-if="showGold">| Gold: {{ pairedPlayers.special_player.gold }}</span>
+                </p>
+            </div>
+            <div class="game__reveal">
+                <button class="game__reveal--gold" @click="showGold=true">Reveal Gold</button>
+            </div>
         </div>
     </section>
 </main>
@@ -14,13 +28,40 @@ import header_file from '../components/header.vue';
 
 export default{
     name: "competitors",
+
     components: {
         header_file,
+    },
+    data(){
+        return {
+            shuffled_players: [],
+            showGold: false,
+        }
     },
     computed: {
         players(){
             return this.$store.state.players
+        },
+        pairedPlayers(){
+            // shuffle players so it will be random who is playing against who.
+            this.shuffled_players = this.players.map(player => ({player, sort: Math.random()}))
+                                         .sort((player, place) => player.sort - place.sort)
+                                         .map(({ player }) => player)
+
+            let pairs = []
+            for(let i=0; i<this.shuffled_players.length-1; i+=2){
+                pairs.push(this.shuffled_players.slice(i, i+2))
+            }
+
+            let special_player = false
+            if(this.shuffled_players.length % 2 !== 0){
+                special_player = this.shuffled_players[this.shuffled_players.length-1]
+            }
+            return {pairs, special_player}
         }
+    },
+    mounted(){
+
     },
     methods: {
 
